2fca3db4808a4a93de15c5592d19538c8c12fb64
Louis Drotos <louis.drotos@gmail.com>
2018-07-29 19:53:20 -0400

    Updated the basemap

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -16,12 +16,16 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements OnInit {
 
+  baseMapUrl = 'https://api.mapbox.com/styles/v1/ldrotos/cjk7ejowm5htm2rl579r0etey/tiles/256/{z}/{x}/{y}';
+  baseMapToken = 'pk.eyJ1IjoibGRyb3RvcyIsImEiOiJwQXgwZ2ZVIn0.pPrIMXZdwniJcp79DNpg9g';
+  baseMapAccess = this.baseMapUrl + '?access_token=' + this.baseMapToken;
+
   options = {
-    layers: [ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })],
-    zoom: 5,
-    center: L.latLng(46.879966, -121.726909)
+    layers: [ L.tileLayer( this.baseMapAccess, { maxZoom: 18, attribution: '...' })],
+    zoom: 10,
+    center: L.latLng(38.8613, -77.2457)
   };
-  
+
   constructor() { }
 
   ngOnInit() {
